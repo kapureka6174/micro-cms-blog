67deda92437471bf2ba0cb1b0f79545c2ed19245
kapureka6174 <mitsu.n.0109@gmail.com>
2022-02-22 18:01:09 +0900

    [update] 常に更新日時も表示されるように変更

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,14 +10,7 @@ const Post = ({ data }) => {
             {emojis[Math.floor(Math.random() * emojis.length)]}
           </div>
           <div className="h-1/4">
-            <div
-              className={
-                'flex items-start pt-2 ' +
-                (data.publishedAt !== data.updatedAt
-                  ? 'justify-evenly'
-                  : 'pl-3')
-              }
-            >
+            <div className="flex items-start justify-evenly pt-2">
               <span className="text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -35,28 +28,25 @@ const Post = ({ data }) => {
                 </svg>
                 {data.publishedAt.substr(0, 10)}
               </span>
-              {data.publishedAt !== data.updatedAt && (
-                <span className="text-slate-500">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mr-1 inline h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                    />
-                  </svg>
-
-                  {data.updatedAt.substr(0, 10)}
-                </span>
-              )}
+              <span className="text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mr-1 inline h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>
+                {data.updatedAt.substr(0, 10)}
+              </span>
             </div>
-            <div className="ml-3 mt-1 w-16 rounded-full bg-indigo-200 px-2 py-1 text-xs text-indigo-500">
+            <div className="ml-3 mt-1 w-16 rounded-full bg-indigo-200 px-2 py-1 text-center text-xs text-indigo-500">
               {data.category}
             </div>
           </div>
